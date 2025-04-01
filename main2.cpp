ffbefc455960e4a1fc7481e4533ee3602d75a3b5
JordanTerwilliger <jordanterwilliger@hotmail.com>
2025-04-01 12:32:32 -0400

    2nd commit

--- main2.cpp
+++ main2.cpp
@@ -7,31 +7,38 @@ int main(){
 
     class ROT{
         private:
-            int* health;
+            int* health; //Pointers since Dynamic Memory Management requires pointers.
             string* name;
         public:
-            //Copy Constructor
+            //Copy Constructor ✔
             //Copy Assignment Constructor ✔
             //Destructor ✔
 
-            ROT(){ //Constructor
+            ROT(){ //Null Constructor
                 health = new int;
                 name = new string;
                 *health = 0;
                 *name = "none";
             }
-            ROT(int health, string name){ //Constructor
+            ROT(int health, const string& name){ //Default Constructor
                 this->health = new int;
                 this->name = new string;
                 *this->health = health;
                 *this->name = name;
             }
 
-            ROT& operator=(const ROT& stamp){ //Copy Assignment Constructor
-                name = new string;
-                health = new int;
-                *health = *(stamp.health);
-                *name = *(stamp.name);
+            ROT(const ROT& stamp) {  // Copy Constructor
+                name = new string(*stamp.name);
+                health = new int(*stamp.health);
+            }
+
+            ROT& operator=(const ROT& stamp){ //Copy Assignment Operator
+                if (this != &stamp){
+                    delete name; 
+                    delete health;
+                    name = new string(*stamp.name);
+                    health = new int(*stamp.health);
+                }
                 return *this;
             }
 
@@ -40,16 +47,34 @@ int main(){
                 delete name;
             }
             void Print(){
-                cout << *name << ": " << *health << endl;
+                cout << getName() << ": " << getHealth() << endl;
                 return;
+            }
+
+            string getName(){
+                return *name;
+            }
+            void setName(string name){
+                *this->name = name;
+            }
+            int getHealth(){
+                return *health;
+            }
+            void setHealth(int health){
+                *this->health = health;
             }
     };
 
     ROT mainCharacter(15, "Jon4");
 
     mainCharacter.Print();
-    while(true);
     
-
+    ROT a(mainCharacter);
+    a.Print();
+    a.setName("Heath");
+    a.setHealth(4);
+    a.Print();
+    a = mainCharacter;
+    a.Print();
     return 0;
 }
